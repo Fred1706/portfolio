da5db4ade584f579cf9b0e8d6b8ca7e3a2a9cdd8
Fred-Eric Auger <fred1942@outlook.fr>
2024-10-16 20:43:52 -0400

    header responsive

--- src/scripts/components/Projets.js
+++ src/scripts/components/Projets.js
@@ -2,34 +2,27 @@ import gsap from 'gsap';
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-export default class Projets{
-    constructor(){
+export default class Projets {
+  constructor() {
+    this.init();
+  }
 
-        this.init();
-        
-    }
-
-
-
-    init(){
-        const container = document.querySelector('.js-horizontal');
-        const containers = gsap.utils.toArray('.js-horizontals');
+  init() {
+    const container = document.querySelector('.js-horizontal');
+    const containers = gsap.utils.toArray('.js-horizontals');
 
     gsap.to(containers, {
       xPercent: -100 * (containers.length - 1),
-      
+
       scrollTrigger: {
         trigger: container,
-        end: "+=" + (window.innerHeight * 7),
+        end: '+=' + window.innerHeight * 7,
         pin: true,
+
         scrub: 1,
       },
     });
-
-
-
-    }
+  }
 }
